55f4cb58b1916698f6560acb3e043703e8704bc9
Santhosh Sundaram <santhoshsundaram9650@gmail.com>
2024-09-01 23:07:15 +0530

    final idaklu working build

--- src/pybammsolvers/__init__.py
+++ src/pybammsolvers/__init__.py
@@ -1,5 +1,5 @@
 from pybammsolvers.solvers.idaklu_jax import IDAKLUJax 
-from pybammsolvers.solvers.idaklu_solver import IDAKLUSolver, have_idaklu, have_iree
+from pybammsolvers.solvers.idaklu_solver import IDAKLUSolver, has_idaklu, has_iree
 
 __all__ = [
     "IDAKLUJax",
