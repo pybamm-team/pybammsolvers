22624a2bba998f8fbb74c56d6f2a6def3c7a6e84
kratman <kratmandu@gmail.com>
2025-02-06 11:55:14 +0000

    Remaining easy cleanup

--- src/pybammsolvers/idaklu_source/IdakluJax.cpp
+++ src/pybammsolvers/idaklu_source/IdakluJax.cpp
@@ -1,14 +1,7 @@
 #include "IdakluJax.hpp"
-
-#include <pybind11/functional.h>
 #include <pybind11/numpy.h>
-#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-
-#include <utility>
-#include <vector>
-#include <iostream>
 #include <functional>
 
 // Initialise static variable
