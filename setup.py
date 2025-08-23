1cadd82073d184e79417a88ef2b0a9cc3d8417a2
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2025-08-23 18:43:50 +0530

    Add pybind11 include dir

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ from setuptools import setup
 from setuptools.command.install import install
 from setuptools.command.bdist_wheel import bdist_wheel
 
+from pybind11 import get_include as pybind11_get_include
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 default_lib_dir = (
@@ -267,6 +268,7 @@ ext_modules = [
             "src/pybammsolvers/idaklu_source/Options.cpp",
             "src/pybammsolvers/idaklu.cpp",
         ],
+        include_dirs=[str(Path(default_lib_dir) / "include"), pybind11_get_include()],
     )
 ]
 
