0bcad90891c95835e1f81d7a4e93dd678ea17506
Saransh Chopra <saransh0701@gmail.com>
2025-12-29 02:10:42 +0100

    fix: include headers in sdist

--- setup.py
+++ setup.py
@@ -300,7 +300,6 @@ ext_modules = [
 # Project metadata was moved to pyproject.toml (which is read by pip). However, custom
 # build commands and setuptools extension modules are still defined here.
 setup(
-    # silence "Package would be ignored" warnings
     include_package_data=True,
     ext_modules=ext_modules,
     cmdclass={
