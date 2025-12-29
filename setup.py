d5d145735db6a371646725a1dc7a4085a1febb49
Saransh Chopra <saransh0701@gmail.com>
2025-12-29 02:20:05 +0100

    include_package_data=False

--- setup.py
+++ setup.py
@@ -300,7 +300,7 @@ ext_modules = [
 # Project metadata was moved to pyproject.toml (which is read by pip). However, custom
 # build commands and setuptools extension modules are still defined here.
 setup(
-    include_package_data=True,
+    include_package_data=False,
     ext_modules=ext_modules,
     cmdclass={
         "build_ext": CMakeBuild,
